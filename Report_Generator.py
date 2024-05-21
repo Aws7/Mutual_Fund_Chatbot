f462af23fe424508ca9711eec1dd5197b54bf4f4
Aws7 <x6@protonmail.com>
2024-05-21 22:31:16 +0300

    Update Report_Generator.py

--- Report_Generator.py
+++ Report_Generator.py
@@ -18,7 +18,6 @@ import re
 import tarfile
 # os.environ['JAVA_HOME'] = './jdk'
 
-
 # if "JAVA_HOME" not in os.environ:
 #     java_url = "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz"
 #     java_tar_gz = wget.download(java_url)
@@ -30,11 +29,7 @@ import tarfile
 #     os.environ["JAVA_HOME"] = java_home
 #     os.environ["PATH"] += os.pathsep + os.path.join(java_home, "bin")
 
-
-
-
-
-st.set_page_config("Report Funds","🤖")
+st.set_page_config("Report Funds", "🤖")
 
 load_dotenv()
 
@@ -43,48 +38,43 @@ load_dotenv()
 COHERE_API_KEY = os.getenv('COHERE_API_KEY')
 
 
-
-
-
-
-#Retrieve Schemes from CSV
+# Retrieve Schemes from CSV
 def schemeRetrieve(path):
-    df=pd.read_csv(path)
-    df.columns=df.iloc[0].values
-    df=df.drop(df.index[0])
-    mf_scheme_list=["All Schemes"]
+    df = pd.read_csv(path)
+    df.columns = df.iloc[0].values
+    df = df.drop(df.index[0])
+    mf_scheme_list = ["All Schemes"]
     for i in df['Scheme Name'].values:
         if "Schemes" in str(i):
             if str(i) not in mf_scheme_list:
-                mf_scheme_list.append(str(i)) 
+                mf_scheme_list.append(str(i))
     return mf_scheme_list
 
+
 def field_retrieve(path):
-    df=pd.read_csv(path)
-    df.columns=df.iloc[0].values
-    df=df.drop(df.index[0])
-    l=df.columns[2:].values
-    ll=[]
+    df = pd.read_csv(path)
+    df.columns = df.iloc[0].values
+    df = df.drop(df.index[0])
+    l = df.columns[2:].values
+    ll = []
     for i in l:
-        newl=i.replace("\r"," ")
-        newl=newl.replace("segregat ed","segregated")
+        newl = i.replace("\r", " ")
+        newl = newl.replace("segregat ed", "segregated")
         if "as" in newl:
-            pos=newl.find("as")
+            pos = newl.find("as")
         if "for" in newl:
-            pos=newl.find("for")
-        newl=newl[0:pos-1]
+            pos = newl.find("for")
+        newl = newl[0:pos - 1]
         ll.append(newl)
     ll.append("Net Inflow or Outflow")
     ll.append("Net Asset under Management per Scheme")
     ll.append("Net Inflow or Outflow per Scheme")
-    
-    return ll
 
+    return ll
 
 
 # Using Cohere's embed-english-v3.0 embedding model
 embeddings = CohereEmbeddings(cohere_api_key=COHERE_API_KEY, model="embed-arabic-v3.0")
-
 
 # For OpenAI's gpt-3.5-turbo llm
 # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
@@ -95,7 +85,7 @@ llm = ChatCohere(temperature=1, cohere_api_key=COHERE_API_KEY, model="command-r"
 
 # For reading PDFs and returning text string
 def read_pdf(files):
-    file_content=''' Net Inflow or Outflow: Calculated as taking the difference of `Funds Mobilized for the month` and `Repurchase/Redemption for the month` field.
+    file_content = ''' Net Inflow or Outflow: Calculated as taking the difference of `Funds Mobilized for the month` and `Repurchase/Redemption for the month` field.
 
      Net Asset under Management per Scheme: Determined by dividing the `Net Assets Under Management` by the `No. of Schemes`.
 
@@ -114,7 +104,7 @@ def read_pdf(files):
     return file_content
 
 
-#Download CSV
+# Download CSV
 def download_df(content, filename='data.csv'):
     df = pd.read_csv(StringIO(content), sep="|", skipinitialspace=True)
 
@@ -123,71 +113,74 @@ def download_df(content, filename='data.csv'):
     df.drop(df.columns[df.columns.str.contains('Unnamed', case=False)], axis=1, inplace=True)
     # csv_bytes = content.encode()  # Convert string to bytes
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()    # Encode bytes to base64
+    b64 = base64.b64encode(csv.encode()).decode()  # Encode bytes to base64
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV File</a>'
     return href
 
 
-#-----------------------------------------------------------#
-#------------------------💬 CHATBOT -----------------------#
-#----------------------------------------------------------#
+# -----------------------------------------------------------#
+# ------------------------💬 CHATBOT -----------------------#
+# ----------------------------------------------------------#
 def chatbot():
     st.subheader("Generate your Desired Mutual Fund Report")
     st.markdown("<br>", unsafe_allow_html=True)
-    btn=st.button("Generate", type="primary")
-    # Check if it is empty
-    if st.session_state.book_docsearch:   
-        # prompt = st.chat_input("Say something")
-        prompt="give the data of "+", ".join(st.session_state.selected_field)
-        
-        # Write previous converstions
-        for i in st.session_state.conversation:
-            user_msg = st.chat_message("human", avatar="🐒")
-            user_msg.write(i[0])
-            computer_msg = st.chat_message("ai", avatar="🧠")
-            computer_msg.write(i[1])
-            
-        if btn and prompt:
-            exprompt=prompt                       #to store the previous prompt
-            exprompt="For the "+", ".join(st.session_state.selected_scheme)+", "+exprompt
-            exprompt+=" . Give the result in tabular format."          #want to show data in tabular form
+    prompt = st.text_input("Enter your query here")
+    btn = st.button("Generate", type="primary")
+
+    # Write previous conversations
+    for i in st.session_state.conversation:
+        user_msg = st.chat_message("human", avatar="🐒")
+        user_msg.write(i[0])
+        computer_msg = st.chat_message("ai", avatar="🧠")
+        computer_msg.write(i[1])
+
+    if btn and prompt:
+        exprompt = prompt  # to store the previous prompt
+
+        if st.session_state.book_docsearch:
+            exprompt = "For the " + ", ".join(st.session_state.selected_scheme) + ", " + exprompt
+            exprompt += " . Give the result in tabular format."  # want to show data in tabular form
             # promptcsv=prompt+" in csv format"   #for downloading csv version                                    
             user_text = f'''Scheme: {", ".join(st.session_state.selected_scheme)}, Field: {", ".join(st.session_state.selected_field)}'''
-            user_msg = st.chat_message("human", avatar="🐒")
-            user_msg.write(user_text)
+        else:
+            user_text = prompt
 
-            with st.spinner("Getting Answer..."):
+        user_msg = st.chat_message("human", avatar="🐒")
+        user_msg.write(user_text)
+
+        with st.spinner("Getting Answer..."):
+            if st.session_state.book_docsearch:
                 # No of chunks the search should retrieve from the db
                 chunks_to_retrieve = 5
-                retriever = st.session_state.book_docsearch.as_retriever(search_type="similarity", search_kwargs={"k":chunks_to_retrieve})
+                retriever = st.session_state.book_docsearch.as_retriever(search_type="similarity", search_kwargs={"k": chunks_to_retrieve})
 
                 ## RetrievalQA Chain ##
                 qa = RetrievalQA.from_llm(llm=llm, retriever=retriever, verbose=True)
                 answer = qa({"query": exprompt})["result"]
-                # answercsv = qa({"query": promptcsv})["result"]    #for generating csv file
-                
-                computer_text = f'''{answer}'''
-                # print(answer)
-                computer_msg = st.chat_message("ai", avatar="🧠") 
-                computer_msg.write(computer_text)
-                
-                #Download Data
+            else:
+                answer = llm(prompt)
+
+            computer_text = f'''{answer}'''
+            # print(answer)
+            computer_msg = st.chat_message("ai", avatar="🧠")
+            computer_msg.write(computer_text)
+
+            if st.session_state.book_docsearch:
+                # Download Data
                 st.markdown(download_df(answer), unsafe_allow_html=True)
-                
+
                 # Showing chunks with score
                 doc_score = st.session_state.book_docsearch.similarity_search_with_score(prompt, k=chunks_to_retrieve)
                 with st.popover("See chunks..."):
                     st.write(doc_score)
-                # Adding current conversation to the list.
-                st.session_state.conversation.append((user_text, answer))   
-    else:
-        st.warning("Please upload a file")
+
+            # Adding current conversation to the list.
+            st.session_state.conversation.append((user_text, answer))
 
 
-            
 # For initialization of session variables
 def initial(flag=False):
-    path="db"
+    path = "db"
     if 'existing_indices' not in st.session_state or flag:
         st.session_state.existing_indices = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
     if ('selected_option' not in st.session_state) or flag:
@@ -195,79 +188,72 @@ def initial(flag=False):
             st.session_state.selected_option = st.session_state.existing_indices[0]
         except:
             st.session_state.selected_option = None
-    
+
     if 'conversation' not in st.session_state:
         st.session_state.conversation = []
     if 'book_docsearch' not in st.session_state:
         st.session_state.book_docsearch = None
-        
-        
+
     if 'mf_schemes' not in st.session_state or flag:
         try:
-            st.session_state.mf_schemes=schemeRetrieve(f"./{path}/table.csv")
+            st.session_state.mf_schemes = schemeRetrieve(f"./{path}/table.csv")
         except:
-            st.session_state.mf_schemes=None
-            
+            st.session_state.mf_schemes = None
+
     if ('selected_scheme' not in st.session_state) or flag:
         try:
             st.session_state.selected_scheme = st.session_state.mf_schemes[0]
         except:
             st.session_state.selected_scheme = None
-    
+
     if 'mf_field' not in st.session_state or flag:
         try:
-            st.session_state.mf_field=field_retrieve(f"./{path}/table.csv")
+            st.session_state.mf_field = field_retrieve(f"./{path}/table.csv")
         except:
-            st.session_state.mf_field=None
-    
+            st.session_state.mf_field = None
+
     if ('selected_field' not in st.session_state) or flag:
         try:
             st.session_state.selected_field = st.session_state.mf_schemes[0]
         except:
             st.session_state.selected_field = None
-            
+
 
 def main():
     initial(True)
     # Streamlit UI
     st.title("💰 Mutual Fund Report Generator")
-    
+
+    # Prompt input at the top for general chat
+    chatbot()
+
     # For showing the index selector
-    file_list=[]
+    file_list = []
     for index in st.session_state.existing_indices:
         with open(f"db/{index}/desc.json", "r") as openfile:
             description = json.load(openfile)
             file_list.append(",".join(description["file_names"]))
-    
-    
 
     with st.popover("Select Index", help="👉 Select the datastore from which data will be retrieved"):
         st.session_state.selected_option = st.radio("Select a Document...", st.session_state.existing_indices, captions=file_list, index=0)
 
     st.write(f"*Selected Index* : **:orange[{st.session_state.selected_option}]**")
-    
+
     if 'existing_indices' in st.session_state:
-        
-        st.session_state.mf_schemes=schemeRetrieve(f"./db/table.csv")
-        
+        st.session_state.mf_schemes = schemeRetrieve(f"./db/table.csv")
+
         with st.popover("Select Scheme", help="👉 Select the Mutual Fund Scheme"):
-        
             radio_scheme = st.multiselect("Select a Scheme...", st.session_state.mf_schemes)
-            # if radio_scheme == "Other (Add Manually)...":
-            #     radio_scheme = st.text_input("Write the name of the Schemes with comma")
             st.session_state.selected_scheme = radio_scheme
-            # st.write(f"You have selected: {st.session_state.selected_scheme}")
-        temp_scheme=", ".join(st.session_state.selected_scheme)
+        temp_scheme = ", ".join(st.session_state.selected_scheme)
         st.write(f"*Selected Scheme* : **:green[{temp_scheme}]**")
-        
+
         with st.popover("Select Field", help="👉 Select the Field"):
-        
             radio_field = st.multiselect("Select a Scheme...", st.session_state.mf_field)
             st.session_state.selected_field = radio_field
-        temp_field=", ".join(st.session_state.selected_field)
-        st.write(f"*Selected Scheme* : **:green[{temp_field}]**")
-        
-    
+        temp_field = ", ".join(st.session_state.selected_field)
+        st.write(f"*Selected Field* : **:green[{temp_field}]**")
+
     # Load the selected index from local storage
     if st.session_state.selected_option:
         st.session_state.book_docsearch = FAISS.load_local(f"db/{st.session_state.selected_option}", embeddings, allow_dangerous_deserialization=True)
@@ -277,10 +263,6 @@ def main():
         st.warning("⚠️ No index present. Please add a new index.")
         st.page_link("pages/Upload_Files.py", label="Upload Files", icon="⬆️")
         st.page_link("pages/Chatbot.py", label="Basic Chatbot", icon="💬")
-        
-            
-            
- 
 
-            
+
 main()
